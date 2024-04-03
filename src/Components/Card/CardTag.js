cf58b40928591cdaf0fbc7cad46ae66cfa1784a3
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:17:46 +0530

    Updated Code

--- src/Components/Card/CardTag.js
+++ src/Components/Card/CardTag.js
@@ -5,7 +5,7 @@ export default function CardTag({tagName, tagColor}) {
     const colorSpan = 
     return (
         <div className={colorDiv}>
-            <span className={`text-lg text-${tagColor}-600 font-semibold`}>{tagName}</span>
+            <span className={}>{tagName}</span>
         </div>
     );
 }
