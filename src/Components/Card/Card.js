603fa6d29304c5259c6349bdee90a8703d768614
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:28:08 +0530

    Updated Code

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import CardImage from "./CardImage";
 import CardBodyContainer from "./CardBodyContainer";
 
-export default function Card({tagName, tagColor, title, description, creatorName, creationDate, creatorImage, thumbnail}) {
+export default function Card({tagName, tagColor, title, description, creatorName, creationDate, creatorImage, thumbnailImage}) {
     return (
         <div className={`w-[30%] h-[38rem] border border-gray-300 rounded-2xl overflow-hidden flex items-start justify-between flex-col`}>
             <CardImage thumbnailImage={thumbnail} />
