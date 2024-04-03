74f88c7539ba181f74a2bab01703049d93792f77
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 14:51:03 +0530

    Updated Code

--- src/Components/Card/CardImage.js
+++ src/Components/Card/CardImage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardImage({flatCard, thumbnailImage, type2}) {
     return (
-        <figure className={`w-full ${flatCard ? 'h-[22rem]'`h-72`} relative`}>
+        <figure className={`w-full ${flatCard ? 'h-[22rem]'`h-72'} relative`}>
             <img
                 className="w-full h-72 object-cover"
                 src={thumbnailImage}
