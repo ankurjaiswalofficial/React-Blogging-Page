58a1dae2a4d39e7ec61413404d4a407030291cc8
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:49:37 +0530

    Updated Code

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,7 @@ import CardBodyContainer from "./CardBodyContainer";
 
 export default function Card({tagName, tagColor, title, description, creatorName, creationDate, creatorImage, thumbnailImage}) {
     return (
-        <div className={`w-[30%] h-[41rem] border border-gray-300 rounded-2xl overflow-hidden flex items-start justify-between flex-col`}>
+        <div className={`w-[30%] h-[4rem] border border-gray-300 rounded-2xl overflow-hidden flex items-start justify-between flex-col`}>
             <CardImage thumbnailImage={thumbnailImage} />
             <CardBodyContainer tagName={tagName} tagColor={tagColor} title={title} description={description} creatorName={creatorName} creatorImage={creatorImage} creationDate={creationDate} />
         </div>
