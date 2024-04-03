7971f7736719c7bb206212b01fbe4aa161f6f912
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:33:10 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -16,34 +16,6 @@ function App() {
         thumbnailImage:
             "https://images.unsplash.com/photo-1647612011547-55ae297d6a97?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
-      {
-        flatCard: false,
-        tagName: "Growth",
-        tagColor: "green",
-        title: "Card Title",
-        description:
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, illo vitae mollitia dolor at itaque odit omnis facilis voluptate impedit.",
-        creatorName: "Ankur Jaiswal",
-        creationDate: "Apr 03, 2024",
-        creatorImage:
-            "https://images.unsplash.com/photo-1712068944613-1ff36db16612?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        thumbnailImage:
-            "https://images.unsplash.com/photo-1647612011547-55ae297d6a97?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    }
-      {
-        flatCard: false,
-        tagName: "Growth",
-        tagColor: "green",
-        title: "Card Title",
-        description:
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, illo vitae mollitia dolor at itaque odit omnis facilis voluptate impedit.",
-        creatorName: "Ankur Jaiswal",
-        creationDate: "Apr 03, 2024",
-        creatorImage:
-            "https://images.unsplash.com/photo-1712068944613-1ff36db16612?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        thumbnailImage:
-            "https://images.unsplash.com/photo-1647612011547-55ae297d6a97?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    }
   ];
     return (
         <div className="max-w-screen-2xl mx-auto">
