f2baad72499b346c7115f30ff6007f09eb440b82
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:16:25 +0530

    Updated Code

--- src/Components/Card/CardTag.js
+++ src/Components/Card/CardTag.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function CardTag({tagName, tagColor}) {
+    
     return (
         <div className={}>
             <span className={`text-lg text-${tagColor}-600 font-semibold`}>{tagName}</span>
