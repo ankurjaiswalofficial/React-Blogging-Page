d2145bcf076e875652e1644308948b918fecc61d
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:34:36 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
                 {cardData.map((e, index) => {
                     return (
                         <Card
-                        key = {String(index) + ""}
+                        key = {String(index) + "e."}
                             flatCard={cardData.flatCard}
                             tagName={cardData.tagName}
                             tagColor={cardData.tagColor}
