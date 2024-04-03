01c75cf6ed801c4a7cb8d9b83ed7d0260f230d9d
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:38:48 +0530

    Updated Code

--- src/Components/Card/CardImage.js
+++ src/Components/Card/CardImage.js
@@ -4,7 +4,7 @@ export default function CardImage({thumbnailImage}) {
     return (
         <figure className="w-full h-auto">
             <img
-                className="w-full h-7 object-cover"
+                className="w-full h-11 object-cover"
                 src={thumbnailImage}
                 alt="Project"
             />
