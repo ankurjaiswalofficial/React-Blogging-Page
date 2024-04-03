e52bcb46fea4b9c9dde1ff0b2cbc3acc18ffa880
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:44:26 +0530

    Updated Code

--- src/Components/Card/CardImage.js
+++ src/Components/Card/CardImage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardImage({thumbnailImage}) {
     return (
-        <figure className="w-full h-[50%]">
+        <figure className="w-full max-h-[50%]">
             <img
                 className="w-full h-full object-cover"
                 src={thumbnailImage}
