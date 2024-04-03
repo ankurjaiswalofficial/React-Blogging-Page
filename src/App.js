508d07c355dc9bf975341bae03b7c688b92cabd2
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:11:17 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             tagColor: "#0af",
             title: "A Practical Guide to Apple Search Ads",
             description:
-                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, illo vitae mollitia dolor at itaque odit omnis facilis voluptate impedit.",
+                "",
             creatorName: "Ankur Jaiswal",
             creationDate: "Apr 03, 2024",
             creatorImage:
