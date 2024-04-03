f0fefac5cace8c4c8b004173f131be4467805430
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:02:10 +0530

    Updated Code

--- src/Components/Card/CardBody.js
+++ src/Components/Card/CardBody.js
@@ -5,7 +5,7 @@ export default function CardBody({tagName, tagColor, title, description}) {
     return (
         <div className="flex flex-col items-start justify-center gap-3 p-3">
             <CardTag tagName={tagName} tagColor={tagColor} />
-            <h5 className={`"font-bold text-2xl`}>
+            <h5 className={`font-bold text-2xl`}>
                 {title}
             </h5>
             <p className="font-normal text-xl">
