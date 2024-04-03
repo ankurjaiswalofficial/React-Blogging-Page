c077f03c74040f54e5f4165fd279d09d3bf9115c
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 14:04:20 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
                     return (
                         <Card
                             key={String(index) + `${e.title}`}
-                            type2={e.type2 != undefined ? e.type2 : false}
+                            type2={e.type2 !== undefined ? e.type2 : false}
                             flatCard={e.flatCard}
                             tagName={e.tagName}
                             tagColor={e.tagColor}
