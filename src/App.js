a69c218f6810b8d660cbd6eb9def9b5c5b1e18b5
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:11:36 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             tagColor: "#0af",
             title: "A Practical Guide to Apple Search Ads",
             description:
-                "Why it's a channel worth exploring to griow",
+                "Why it's a channel worth exploring to grow",
             creatorName: "Ankur Jaiswal",
             creationDate: "Apr 03, 2024",
             creatorImage:
