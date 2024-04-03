9888f98b9edb43569df2bdc7c66552923331a6a9
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:15:50 +0530

    Updated Code

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,7 @@ import CardBodyContainer from "./CardBodyContainer";
 
 export default function Card({tagName, tagColor, title, description, creatorName, creationDate, creatorImage, thumbnailImage, flatCard=false, type2=false}) {
     return (
-        <div className={`flex items-start justify-between ${flatCard ? 'flex-row w-full h-[22rem] my-8' : 'flex-col w-[30%] h-[40rem] border border-gray-300 rounded-2xl '} overflow-hidden`}>
+        <div className={`flex items-start justify-between ${flatCard ? 'flex-row w-full h-[26rem] my-8' : 'flex-col w-[30%] h-[40rem] border border-gray-300 rounded-2xl '} overflow-hidden`}>
             <CardImage flatCard={flatCard} thumbnailImage={thumbnailImage} type2={type2} />
             <CardBodyContainer flatCard={flatCard} tagName={tagName} tagColor={tagColor} title={title} description={description} creatorName={creatorName} creatorImage={creatorImage} creationDate={creationDate} />
         </div>
