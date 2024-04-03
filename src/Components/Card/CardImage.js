631349f893988ac5b6a2ae5cbe0d60cd40420ee8
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:16:36 +0530

    Updated Code

---
+++ src/Components/Card/CardImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CardImage({flatCard, thumbnailImage, type2}) {
+    return (
+        <figure className={`${flatCard ? 'w-h-full rounded-2xl' : 'w-full h-72'} relative`}>
+            <img
+                className={`w-full ${flatCard ? 'h-full rounded-2xl' : 'h-72'} object-cover`}
+                src={thumbnailImage}
+                alt="Project"
+            />
+        </figure>
+    );
+}
