67164f26e2a9b213afbafa59ebdd05b8c83f9211
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:01:53 +0530

    Updated Code

--- src/Components/Card/CardBodyContainer.js
+++ src/Components/Card/CardBodyContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import CardFooter from "./CardFooter";
 import CardBody from "./CardBody";
 
-export default function CardBodyContainer({tagName, tagColor, title, description, creatorName, creatorImage, creationDate}) {
+export default function CardBodyContainer({flatCard, tagName, tagColor, title, description, creatorName, creatorImage, creationDate}) {
     return (
         <div className="w-full h-full flex flex-col items-start justify-between p-3 gap-2">
             <CardBody flatCard={flatCard} tagName={tagName} tagColor={tagColor} title={title} description={description} />
