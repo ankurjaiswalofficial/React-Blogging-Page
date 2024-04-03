51e268132088f16e715eec7332a98ce7120b6cc4
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 14:02:37 +0530

    Updated Code

--- src/Components/Card/CardImage.js
+++ src/Components/Card/CardImage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function CardImage({thumbnailImage, type2}) {
+export default function CardImage({flatCard, thumbnailImage, type2}) {
     return (
         <figure className="w-full h-auto">
             <img
