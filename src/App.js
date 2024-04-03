4dbfd0c6598c9255f82ac9c3c6f81ebe521cc0a0
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:11:53 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             description:
                 "Why it's a channel worth exploring to grow your app",
             creatorName: "Thomas Petit",
-            creationDate: "Apr 03, 2024",
+            creationDate: "June",
             creatorImage:
                 "https://images.unsplash.com/photo-1712068944613-1ff36db16612?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             thumbnailImage:
