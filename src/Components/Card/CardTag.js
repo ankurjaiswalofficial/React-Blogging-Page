7bb3f04928f9f02e1927e656535421a2061b1ffd
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:22:10 +0530

    Updated Code

--- src/Components/Card/CardTag.js
+++ src/Components/Card/CardTag.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 export default function CardTag({tagName, tagColor}) {
-    const colorDiv = `p-1 px-4 border-2 border-${tagColor}-600 w-fit rounded-full`;
-    const colorSpan = `text-lg text-${tagColor}-600 font-semibold`;
     return (
-        <div className={colorDiv}>
-            <span className={colorSpan}>{tagName}</span>
+        <div className={`p-1 px-4 border-2 border-${tagColor}-600 w-fit rounded-full`}>
+            <span className={`text-lg text-${tagColor}-600 font-semibold`}>{tagName}</span>
         </div>
     );
 }
