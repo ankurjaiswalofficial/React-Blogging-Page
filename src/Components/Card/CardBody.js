e5b6eac0bb316ed1c8a07adb9af796ec8b49de0d
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 15:08:43 +0530

    Updated Code

--- src/Components/Card/CardBody.js
+++ src/Components/Card/CardBody.js
@@ -3,7 +3,7 @@ import CardTag from "./CardTag";
 
 export default function CardBody({flatCard, tagName, tagColor, title, description}) {
     return (
-        <div className={`flex flex-col items-start justify-center ${flatCard ? ''} p-3`}>
+        <div className={`flex flex-col items-start justify-center ${flatCard ? '' : 'gap-3'} p-3`}>
             <CardTag flatCard={flatCard} tagName={tagName} tagColor={tagColor} />
             <h5 className={`font-bold ${flatCard ? 'text-4xl' : 'text-2xl'}`}>
                 {title}
