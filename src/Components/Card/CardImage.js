991b818472bdcced93c37fe3b380e284c014f94f
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 14:37:29 +0530

    Updated Code

--- src/Components/Card/CardImage.js
+++ src/Components/Card/CardImage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardImage({flatCard, thumbnailImage, type2}) {
     return (
-        <figure className={"w-full h-auto relative oveflo"}>
+        <figure className={"w-full h-auto relative overflow-hidden"}>
             <img
                 className="w-full h-72 object-cover"
                 src={thumbnailImage}
