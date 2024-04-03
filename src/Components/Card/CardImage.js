52f321626481aaa8024e8e161d0db9e6cc40caee
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 14:55:37 +0530

    Updated Code

--- src/Components/Card/CardImage.js
+++ src/Components/Card/CardImage.js
@@ -4,7 +4,7 @@ export default function CardImage({flatCard, thumbnailImage, type2}) {
     return (
         <figure className={`w-full ${flatCard ? 'h-[22rem] rounded-2xl' : 'h-72'} relative`}>
             <img
-                className={`w-full ${flatCard ? 'h-[22rem] rounded-2xl' : 'h-72'} relative`}
+                className={`w-full ${flatCard ? 'h-[22rem] rounded-2xl' : 'h-72'} object-fit`}
                 src={thumbnailImage}
                 alt="Project"
             />
