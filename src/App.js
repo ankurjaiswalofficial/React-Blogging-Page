0b4866e3191558bf0a057bb2619cc09ef0df47ac
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-03 13:55:35 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
             creatorImage:
                 "https://images.unsplash.com/photo-1523779917675-b6ed3a42a561?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             thumbnailImage:
-                "https://images.unsplash.com/photo-1638136264464-2711f0078d1e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
     ];
     return (
